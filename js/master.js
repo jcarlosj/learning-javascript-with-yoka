ae7ccc348a973ea0b575a4ed25fa6786c8591a1b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-24 09:34:26 -0500

    [+] Declaraciones var, let const

--- js/master.js
+++ js/master.js
@@ -1,6 +1,27 @@
-/** JavaScript Embebido en el Cuerpo del DOM */
-name = 'Nutella';       // Asginar un valor
-console .log( name );
-name = 12.6;
-name = true;
-console .log( name );
+/** Scope */
+/** Funciona pero preferiblemente evitarla */
+petName = 'Nutella';
+petName = 'Rex';
+console.log( petName );
+
+// CORRECTO
+var petName = 'Nutella';
+var petName = 'Rex';
+console.log( petName );
+
+/** CORRECTA */
+let name = 'Juan';
+name = 'Yoka';      // El valor no se puede declarar dos o mas veces
+console.log( name );
+
+/** INCORRECTA */
+let name = 'Juan';
+let name = 'Yoka';      // El valor no se puede declarar dos o mas veces
+console.log( name );
+
+/** Constantes: Valores que no cambian */
+const PI = 3.1415;      // CORRECTA
+
+// INCORRECTO
+const PI;      
+PI = 3.1415;
